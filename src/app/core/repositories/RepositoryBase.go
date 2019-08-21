341a20b494376760a503774031e12224f762fe63
nguyenhoaikhan <hoaikhan170797@gmail.com>
2019-08-21 08:26:55 +0700

    repo base update

--- src/app/core/repositories/RepositoryBase.go
+++ src/app/core/repositories/RepositoryBase.go
@@ -2,35 +2,50 @@ package repositories
 
 import (
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+// RepositoryBase is a
 type RepositoryBase struct {
 	model *mgo.Collection
 }
 
+// Init is a
 func (re *RepositoryBase) Init(model *mgo.Collection) {
 	re.model = model
 
 }
 
+// Create is a
 func (re *RepositoryBase) Create(user interface{}) error {
 	err := re.model.Insert(user)
 	return err
 }
 
-func (re *RepositoryBase) Get() (result interface{}) {
+// GetByID is a
+func (re *RepositoryBase) GetByID(_id bson.ObjectId) (result interface{}, err error) {
+	err = re.model.Find(bson.M{"_id": _id}).One(&result)
+	return
+}
+
+// GetAll is a
+func (re *RepositoryBase) GetAll() (result []interface{}) {
+	iter := re.model.Find(nil).Iter()
+	var doc interface{}
+	for iter.Next(&doc) {
+		result = append(result, doc)
+	}
 	return result
 }
 
-func (re *RepositoryBase) GetAll() (result interface{}) {
-	result = re.model
-	return result
-}
-
+// Update is a
 func (re *RepositoryBase) Update(updateUser interface{}) (result interface{}) {
 	return result
 }
 
-func (re *RepositoryBase) Delete(_id interface{}) (result interface{}) {
-	return result
+// Delete is a
+func (re *RepositoryBase) Delete(_id bson.ObjectId) (err error) {
+	err = re.model.Remove(bson.M{"_id": _id})
+	return
+
 }
